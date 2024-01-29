6650700b9e7f1d2032e4a26ec7b0de3babab121f
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2024-01-28 23:14:17 -0800

    Make close-dialog button functional

--- src/popup.js
+++ src/popup.js
@@ -4,4 +4,8 @@ const togglePopup = () => {
   popup.classList.toggle("hidden");
 };
 
+const againBtn = popup.querySelector("button");
+
+againBtn.addEventListener("click", togglePopup);
+
 export { togglePopup };
